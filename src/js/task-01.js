c9945833d10ff08755e4ecab179c7bf44f7bc9f2
Vladimir Kravchenko <kvv1793@gmail.com>
2021-04-28 18:06:56 +0300

    HW2

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const liItems = document.querySelectorAll('li.item');
+ console.log(`В ${liItems.length} категории.`)
+
+
+liItems.forEach(liItem => {
+    const h2 = liItem.querySelector('h2').textContent;
+    const ul = liItem.querySelectorAll('li').length;
+    console.log(`Категория: ${h2}`);
+    console.log(`Количество элементов: ${ul}`);
+
+});
